96d84ea76e67da322df167a01360902d6e7a0837
Gaetano Coppoletta s301131 <s301131@studenti.polito.it>
2022-03-09 17:08:53 +0100

    completed exercise1

--- week1/Exercise1.js
+++ week1/Exercise1.js
@@ -12,15 +12,11 @@ const new_scores =[...scores] //copy of the array scores
 let min_pos = 0;
 
 for(let i=0;i<new_scores.length;i++){
-    if(scores[i]<scores[min_pos])
+    if(new_scores[i]<new_scores[min_pos])
         min_pos=i;
 }
 
 new_scores.splice(min_pos,1) //it removes one element at pos= min_pos
-
-
-console.log(new_scores.join(', '));
-
 
 min_pos = 0;
 
@@ -33,5 +29,16 @@ for(let i=0;i<new_scores.length;i++){
 
 new_scores.splice(min_pos,1) //it removes one element at pos= min_pos
 
+
+//compute the average over new_scores
+let new_avg=0;
+for (let x of new_scores){
+    new_avg+=x;
+}
+new_avg /=new_scores.length;
+
+//add two new scores equal to the avg
+new_scores.push(Math.round(new_avg));
+new_scores.push(Math.round(new_avg));
 
 console.log(new_scores.join(', '));
