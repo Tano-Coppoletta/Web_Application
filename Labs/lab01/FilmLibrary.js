4784a205f362e420fe6e16395a1d542e03fc10d2
Gaetano Coppoletta s301131 <s301131@studenti.polito.it>
2022-03-10 18:23:38 +0100

    Exercise1 and beginning of filmLibrary

---
+++ Labs/lab01/FilmLibrary.js
@@ -0,0 +1,11 @@
+'use strict'
+
+function myFilms(id,title,favorite,date,rating){
+    return {
+        id: id,
+        title: title,
+        favorite: favorite,
+        date: date,
+        rating:rating
+    }
+}
